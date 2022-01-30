98afefe4900aa593ed1331ae30a19c47d29c7dd7
kakasoo <kscodebase@gmail.com>
2022-01-30 15:04:10 +0900

    setting :스로틀러 모듈 추가 #0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [ThrottlerModule.forRoot({ ttl: 60, limit: 10 })],
   controllers: [AppController],
   providers: [AppService],
 })
