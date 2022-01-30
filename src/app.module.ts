3c5b45750d6a1f3d5c3b1a578879a9a2a449f841
kakasoo <kscodebase@gmail.com>
2022-01-30 15:04:52 +0900

    setting : 스로틀러를 전역으로 설정 #0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module } from '@nestjs/common';
-import { ThrottlerModule } from '@nestjs/throttler';
+import { APP_GUARD } from '@nestjs/core';
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [ThrottlerModule.forRoot({ ttl: 60, limit: 10 })],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
