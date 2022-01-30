587354c1f6b79293a8afb351a85765eb0394a099
kakasoo <kscodebase@gmail.com>
2022-01-30 18:37:57 +0900

    setting : typeorm에 대한 설정 파일 추가 #0

---
+++ src/config/typeorm/index.ts
@@ -0,0 +1,26 @@
+import { ConfigModule, ConfigService } from '@nestjs/config';
+
+export const databaseProviders = [
+  {
+    imports: [ConfigModule],
+    inject: [ConfigService],
+    useFactory: async (configService: ConfigService) => {
+      const NODE_ENV = configService.get('NODE_ENV');
+
+      return {
+        type: process.env.DB_TYPE,
+        host: configService.get(`${NODE_ENV}_DB_HOST`),
+        port: Number(configService.get<number>(`${NODE_ENV}_DB_PORT`)),
+        username: configService.get(`${NODE_ENV}_DB_USERNAME`),
+        database: configService.get(`${NODE_ENV}_DB_DATABASE`),
+        password: configService.get(`${NODE_ENV}_DB_PASSWORD`),
+        entities: [__dirname + '../../**/*.entity{.ts,.js}'],
+        synchronize: false,
+
+        ...(NODE_ENV === 'DEVELOPMENT'
+          ? { retryAttempts: 2, logging: true }
+          : { logging: false }),
+      };
+    },
+  },
+];
