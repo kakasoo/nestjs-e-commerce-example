0cdbc32492ebe3647ea891e276a38a401ac33fac
kakasoo <kscodebase@gmail.com>
2022-01-30 15:39:33 +0900

    setting : http-exceiption filter 정의 #0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
+import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HttpExceptionFilter } from './common/filters/http-exception.fiter';
 import { TimeoutInterceptor } from './common/interceptors/timout.interceptor';
 
 @Module({
@@ -17,6 +18,10 @@ import { TimeoutInterceptor } from './common/interceptors/timout.interceptor';
     {
       provide: APP_INTERCEPTOR,
       useClass: TimeoutInterceptor,
+    },
+    {
+      provide: APP_FILTER,
+      useClass: HttpExceptionFilter,
     },
   ],
 })
