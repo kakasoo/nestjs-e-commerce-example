4687d115f3b30918fdf4428891a0c9d997e34f53
kakasoo <kscodebase@gmail.com>
2022-01-30 16:09:34 +0900

    setting : nestia 설정 및 package script 추가, Samchon님의 백엔드 구조 반영 #0

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { APP_FILTER, APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
-import { AppController } from './app.controller';
+import { AppController } from './controllers/app.controller';
 import { AppService } from './app.service';
 import { HttpExceptionFilter } from './common/filters/http-exception.fiter';
 import { TimeoutInterceptor } from './common/interceptors/timout.interceptor';
