572d2f72273150d91c1ebda4aa0b09167ca5edf4
kakasoo <kscodebase@gmail.com>
2022-01-30 15:36:55 +0900

    setting : timoutInterceptor 전역 범위로 설정 #0
    - AppModule Provider로 넣어야 injection이 가능하다.

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
-import { APP_GUARD } from '@nestjs/core';
+import { APP_GUARD, APP_INTERCEPTOR } from '@nestjs/core';
 import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TimeoutInterceptor } from './common/interceptors/timout.interceptor';
 
 @Module({
   imports: [ThrottlerModule.forRoot({ ttl: 60, limit: 10 })],
@@ -12,6 +13,10 @@ import { AppService } from './app.service';
     {
       provide: APP_GUARD,
       useClass: ThrottlerGuard,
+    },
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: TimeoutInterceptor,
     },
   ],
 })
