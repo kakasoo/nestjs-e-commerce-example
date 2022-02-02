e848b6de1d5206f3a18cc838848a5a224564b32d
kakasoo <kscodebase@gmail.com>
2022-02-02 18:08:55 +0900

    chore : nestia api 문서의 dto를 별도 폴더로 재 이전

---
+++ src/dtos/create-user.dto.ts
@@ -0,0 +1,15 @@
+import { PickType } from '@nestjs/swagger';
+import { User } from '../models/tables/user';
+
+export class CreateUserDto extends PickType(User, [
+  'oauthId',
+  'name',
+  'nickname',
+  'profileImage',
+  'phoneNumber',
+  'email',
+  'birth',
+  'gender',
+  'smsAdsConsent',
+  'emailAdsConsent',
+] as const) {}
