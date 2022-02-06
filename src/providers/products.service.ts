67b935b9ba65c9f0f14527da6460e71e98d274cb
kakasoo <kscodebase@gmail.com>
2022-02-06 15:26:49 +0900

    feat : 상품의 상세 내역을 가져오는 서비스를 작성
    - 헤더 이미지
    - 상세 이미지
    - 카테고리
    - 옵션들 ( 삭제되지 않았고 승인된 상품 옵션들 )
    - 상품 ( 판매, 품절 : 삭제되든 안 됐든 승인된 상품 )

--- src/providers/products.service.ts
+++ src/providers/products.service.ts
@@ -9,4 +9,28 @@ export class ProductsService {
     @InjectRepository(Product)
     private readonly productsRepository: Repository<Product>,
   ) {}
+
+  async getDetail(productId: number) {
+    return await this.productsRepository
+      .createQueryBuilder('product')
+      .withDeleted()
+      .leftJoinAndMapMany('prouct.headers', 'product.headers', 'header')
+      .leftJoinAndMapMany('product.bodies', 'product.bodies', 'body')
+      .leftJoinAndMapMany(
+        'product.categories',
+        'product.categories',
+        'category',
+        'category.deletedAt IS NULL',
+      )
+      .leftJoinAndMapMany(
+        'product.options',
+        'product.options',
+        'option',
+        'option.deletedAt IS NULL AND option.isSale = :isSale',
+        { isSale: true },
+      )
+      .where('product.id = :productId', { productId })
+      .andWhere('product.isSale = :isSale', { isSale: true })
+      .getOne();
+  }
 }
