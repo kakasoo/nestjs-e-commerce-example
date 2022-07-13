9f0bae31bc0e82d625b8613c6349cc237b940349
kakasoo <kscodebase@gmail.com>
2022-07-13 23:22:53 +0900

    docs : login에 대한 설명 추가

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,10 +1,12 @@
 import { Controller, Request, Post, UseGuards } from '@nestjs/common';
-import { ApiBody } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { LoginUserDto } from '@root/models/dtos/login-user.dto';
 import { LocalGuard } from './guards/local.guard';
 
+@ApiTags('권한 / Auth')
 @Controller()
 export class AuthController {
+  @ApiOperation({ summary: '이메일과 패스워드를 이용한 로그인' })
   @ApiBody({ type: LoginUserDto })
   @UseGuards(LocalGuard)
   @Post('auth/login')
